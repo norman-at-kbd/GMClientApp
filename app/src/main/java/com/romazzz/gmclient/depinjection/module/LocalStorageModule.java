569b73ded781c48a6ce30888cb49faa72843c3da
romaz63n700 <zaytcev.roman@gmail.com>
2017-04-06 21:43:46 +0300

    working on basic mail client components structure

---
+++ app/src/main/java/com/romazzz/gmclient/depinjection/module/LocalStorageModule.java
@@ -0,0 +1,8 @@
+package com.romazzz.gmclient.depinjection.module;
+
+/**
+ * Created by z01tan on 4/6/17.
+ */
+
+public class LocalStorageModule {
+}
---
+++ app/src/main/java/com/romazzz/gmclient/mailclient/network/INetworkMailClient.java
@@ -0,0 +1,8 @@
+package com.romazzz.gmclient.mailclient.network;
+
+/**
+ * Created by z01tan on 4/6/17.
+ */
+
+public interface INetworkMailClient {
+}
