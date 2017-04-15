51114ecc211cdaa327e0a6c9f4db232d50068cc0
romaz63n700 <zaytcev.roman@gmail.com>
2017-04-15 19:12:34 +0300

    IMessage interface changed

--- app/src/main/java/com/romazzz/gmclient/mailclient/IMessage.java
+++ app/src/main/java/com/romazzz/gmclient/mailclient/IMessage.java
@@ -5,4 +5,10 @@ package com.romazzz.gmclient.mailclient;
  */
 
 public interface IMessage {
+    boolean isSent();
+    boolean isUnread();
+    String getFrom();
+    String getTo();
+    String getSubject();
+    String getText();
 }
