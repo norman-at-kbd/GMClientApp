4e76063b947de3f317077cf32ed3e28ff410916c
romazzz <zaytcev.roman@gmail.com>
2017-04-16 00:08:19 +0300

    tests for message storage

--- app/src/test/java/com/romazzz/gmclient/mailclient/localclient/MessageStorageTest.java
+++ app/src/test/java/com/romazzz/gmclient/mailclient/localclient/MessageStorageTest.java
@@ -1,12 +1,17 @@
 package com.romazzz.gmclient.mailclient.localclient;
 
 import com.romazzz.gmclient.mailclient.IMessage;
+import com.romazzz.gmclient.mailclient.Message;
 import com.romazzz.gmclient.mailclient.localclient.IMessageStorage;
 import com.romazzz.gmclient.mailclient.localclient.MessageStorage;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -14,60 +19,51 @@ import static org.junit.Assert.*;
  * Created by z01tan on 4/13/17.
  */
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MessageStorageTest {
-    IMessageStorage storage;
-    class TestMessage implements IMessage {
-        @Override
-        public boolean isSent() {
-            return false;
-        }
-
-        @Override
-        public boolean isUnread() {
-            return false;
-        }
-
-        @Override
-        public String getFrom() {
-            return null;
-        }
-
-        @Override
-        public String getTo() {
-            return null;
-        }
-
-        @Override
-        public String getSubject() {
-            return null;
-        }
-
-        @Override
-        public String getText() {
-            return null;
-        }
-    }
+    final String from = "from";
+    final String to = "to";
+    final String subject = "subject";
+    final String text = "text";
+    final String from2 = "from";
+    final String to2 = "to";
+    final String subject2 = "subject";
+    final String text2 = "text";
+    IMessage message1;
+    IMessage message2;
+    IMessage message3;
 
     @Before
     public void initMessageStorage() {
-        storage = new MessageStorage();
+        message1 = new Message(from,to,subject,text);
+        message2 = new Message(from2, to2, subject2, text2);
+        message3 = new Message(from, to, subject, text);
     }
 
     @Test
-    public void testSave() {
+    public void testASave() {
+        IMessageStorage storage = new MessageStorage();
         int before = storage.getMessages().size();
-        storage.saveMessage(new TestMessage());
+        storage.saveMessage(message1);
         int after = storage.getMessages().size();
         assertEquals(before + 1, after);
+        assertTrue(storage.getMessages().contains(message1));
+        //check that same message won' be saved twice
+        storage.saveMessage(message1);
+        assertEquals(storage.getMessages().size(), after);
     }
 
     @Test
-    public void testMarkSent() {
-
+    public void testBDelete() {
+        IMessageStorage storage = new MessageStorage();
+        storage.saveMessage(message1);
+        storage.saveMessage(message2);
+        storage.delete(message1);
+        assertTrue(!storage.getMessages().contains(message1));
     }
 
     @Test
-    public void testMarkDeleted() {
+    public void testCMarkSent() {
 
     }
 
