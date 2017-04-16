70c1e2196942ad6e2916cd3cb09f6cfb84086243
romaz63n700 <zaytcev.roman@gmail.com>
2017-04-16 22:43:34 +0300

    network client implements interface changes

--- app/src/main/java/com/romazzz/gmclient/mailclient/network/NetworkMailClient.java
+++ app/src/main/java/com/romazzz/gmclient/mailclient/network/NetworkMailClient.java
@@ -2,6 +2,8 @@ package com.romazzz.gmclient.mailclient.network;
 
 import com.romazzz.gmclient.mailclient.IMessage;
 
+import java.util.List;
+
 /**
  * Created by z01tan on 4/16/17.
  */
@@ -15,5 +17,15 @@ public class NetworkMailClient implements INetworkMailClient {
     @Override
     public void delete(String messageID) {
 
+    }
+
+    @Override
+    public void login() {
+
+    }
+
+    @Override
+    public List<IMessage> getList() {
+        return null;
     }
 }
