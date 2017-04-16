23547b0f1eed73f56379146d96fce93d959d4b07
romaz63n700 <zaytcev.roman@gmail.com>
2017-04-16 18:36:42 +0300

    proper naming for depgraph elements

--- app/src/main/java/com/romazzz/gmclient/depinjection/component/MessageStorageComponent.java
+++ app/src/main/java/com/romazzz/gmclient/depinjection/component/MessageStorageComponent.java
@@ -4,5 +4,5 @@ package com.romazzz.gmclient.depinjection.component;
  * Created by z01tan on 4/6/17.
  */
 
-public interface LocalStorageComponent {
+public interface MessageStorageComponent {
 }
--- app/src/main/java/com/romazzz/gmclient/depinjection/module/MessageStorageModule.java
+++ app/src/main/java/com/romazzz/gmclient/depinjection/module/MessageStorageModule.java
@@ -4,5 +4,5 @@ package com.romazzz.gmclient.depinjection.module;
  * Created by z01tan on 4/6/17.
  */
 
-public class LocalStorageModule {
+public class MessageStorageModule {
 }
