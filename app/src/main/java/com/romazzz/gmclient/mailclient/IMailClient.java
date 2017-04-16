7e7c6d8145292b9a01ba79e8dadce52034059e8f
romaz63n700 <zaytcev.roman@gmail.com>
2017-04-16 21:06:25 +0300

    now IMailClient and INetworkMailClien are inherited from ICommonMailClient

--- app/src/main/java/com/romazzz/gmclient/mailclient/IMailClient.java
+++ app/src/main/java/com/romazzz/gmclient/mailclient/IMailClient.java
@@ -6,8 +6,5 @@ import java.util.List;
  * Created by z01tan on 4/6/17.
  */
 
-public interface IMailClient {
-    void login();
-    void send(IMessage message);
-    List<IMessage> getList();
+public interface IMailClient extends ICommonMailClient{
 }
--- app/src/main/java/com/romazzz/gmclient/mailclient/network/INetworkMailClient.java
+++ app/src/main/java/com/romazzz/gmclient/mailclient/network/INetworkMailClient.java
@@ -1,12 +1,12 @@
 package com.romazzz.gmclient.mailclient.network;
 
+import com.romazzz.gmclient.mailclient.ICommonMailClient;
 import com.romazzz.gmclient.mailclient.IMessage;
 
 /**
  * Created by z01tan on 4/6/17.
  */
 
-public interface INetworkMailClient {
-    void send(IMessage message);
+public interface INetworkMailClient extends ICommonMailClient{
     void delete(String messageID);
 }
