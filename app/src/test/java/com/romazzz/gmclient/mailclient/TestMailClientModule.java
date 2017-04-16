2accaa9c6a83328ea77a8c51bc02ed61be5d8092
romaz63n700 <zaytcev.roman@gmail.com>
2017-04-16 22:45:31 +0300

    mailclient module extension for test purposes

---
+++ app/src/test/java/com/romazzz/gmclient/mailclient/TestMailClientModule.java
@@ -0,0 +1,22 @@
+package com.romazzz.gmclient.mailclient;
+
+import com.romazzz.gmclient.depinjection.module.MailClientModule;
+import com.romazzz.gmclient.mailclient.localclient.IMessageStorage;
+import com.romazzz.gmclient.mailclient.network.INetworkMailClient;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by z01tan on 4/16/17.
+ */
+
+@Module
+public class TestMailClientModule extends MailClientModule {
+    @Provides
+    @Override
+    protected IMailClient provideMailClient(INetworkMailClient networkMailClient,
+                                            IMessageStorage messageStorage) {
+        return super.provideMailClient(networkMailClient, messageStorage);
+    }
+}
