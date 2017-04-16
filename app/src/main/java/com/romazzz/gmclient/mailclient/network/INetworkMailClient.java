24cae8acb7cec956ba19725ea54db3e60dd54a3c
romaz63n700 <zaytcev.roman@gmail.com>
2017-04-16 19:27:52 +0300

    basic draft for networkmailclient

--- app/src/main/java/com/romazzz/gmclient/mailclient/network/INetworkMailClient.java
+++ app/src/main/java/com/romazzz/gmclient/mailclient/network/INetworkMailClient.java
@@ -1,8 +1,12 @@
 package com.romazzz.gmclient.mailclient.network;
 
+import com.romazzz.gmclient.mailclient.IMessage;
+
 /**
  * Created by z01tan on 4/6/17.
  */
 
 public interface INetworkMailClient {
+    void send(IMessage message);
+    void delete(String messageID);
 }
---
+++ app/src/main/java/com/romazzz/gmclient/mailclient/network/NetworkMailClient.java
@@ -0,0 +1,19 @@
+package com.romazzz.gmclient.mailclient.network;
+
+import com.romazzz.gmclient.mailclient.IMessage;
+
+/**
+ * Created by z01tan on 4/16/17.
+ */
+
+public class NetworkMailClient implements INetworkMailClient {
+    @Override
+    public void send(IMessage message) {
+
+    }
+
+    @Override
+    public void delete(String messageID) {
+
+    }
+}
