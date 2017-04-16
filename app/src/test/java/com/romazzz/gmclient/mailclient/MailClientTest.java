8304c3d2254a551d02dd71e0d107941315e16c81
romaz63n700 <zaytcev.roman@gmail.com>
2017-04-16 22:47:39 +0300

    MailCien test with test modules injection

--- app/src/test/java/com/romazzz/gmclient/mailclient/MailClientTest.java
+++ app/src/test/java/com/romazzz/gmclient/mailclient/MailClientTest.java
@@ -1,19 +1,67 @@
 package com.romazzz.gmclient.mailclient;
 
 import com.romazzz.gmclient.depinjection.component.DaggerMailClientComponent;
+import com.romazzz.gmclient.depinjection.module.MailClientModule;
+import com.romazzz.gmclient.mailclient.localclient.IMessageStorage;
+import com.romazzz.gmclient.mailclient.network.INetworkMailClient;
+import com.romazzz.gmclient.mailclient.network.NetworkMailClient;
 
 import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by z01tan on 4/16/17.
  */
 
 public class MailClientTest {
+    public boolean isLoginCalled = false;
+    public boolean isMailSent = false;
+
+    class TestNetworkClient extends NetworkMailClient {
+
+        @Override
+        public void login() {
+            isLoginCalled = true;
+        }
+
+        @Override
+        public void send(IMessage message) {
+            isMailSent = true;
+        }
+    }
+
+    class TestMailClient extends MailClient {
+        public TestMailClient(INetworkMailClient networkMailClient, IMessageStorage storage) {
+            super(networkMailClient, storage);
+        }
+
+        @Override
+        public INetworkMailClient getmNetworkMailClient() {
+            return super.getmNetworkMailClient();
+        }
+
+        @Override
+        public IMessageStorage getmMessageStorage() {
+            return super.getmMessageStorage();
+        }
+    }
 
     @Test
     public void dependencyGraphTest() {
-        IMailClient mailClient= DaggerMailClientComponent.
-                builder().build().getMailClient();
+        TestNetworkMailClientModule mockedNetMailClientModule = mock(TestNetworkMailClientModule.class);
+        when(mockedNetMailClientModule.provideNetworkMailClient()).thenReturn(new TestNetworkClient());
 
+//        TestMailClientModule mockMailClientModule = mock(TestMailClientModule.class);
+//        when(mockMailClientModule.provideMailClient(INetworkMailClient network, ))
+        IMailClient mailClient= DaggerMailClientComponent.
+                builder().networkMailClientModule(mockedNetMailClientModule).build().getMailClient();
+        mailClient.login();
+        mailClient.send(new Message("f","t","s","t"));
+        assertTrue(isLoginCalled);
+        assertTrue(isMailSent);
     }
 }
