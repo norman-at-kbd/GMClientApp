3477e62b128326269f61792e81ade718323a03bc
romaz63n700 <zaytcev.roman@gmail.com>
2017-05-15 22:09:44 +0300

    adding view interfaces for mvp

---
+++ app/src/main/java/com/romazzz/gmclient/ui/IMVPView.java
@@ -0,0 +1,10 @@
+package com.romazzz.gmclient.ui;
+
+/**
+ * Created by z01tan on 5/15/17.
+ */
+
+public interface IMVPView {
+
+    void showConnectionError();
+}
---
+++ app/src/main/java/com/romazzz/gmclient/ui/login/ILoginView.java
@@ -0,0 +1,10 @@
+package com.romazzz.gmclient.ui.login;
+
+import com.romazzz.gmclient.ui.IMVPView;
+
+/**
+ * Created by z01tan on 5/15/17.
+ */
+
+public interface ILoginView extends IMVPView {
+}
