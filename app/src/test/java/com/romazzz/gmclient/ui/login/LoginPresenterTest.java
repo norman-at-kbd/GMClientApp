305ac20b9c8f6c96e3029fcb99c60b3ac6dadd67
romazzz <zaytcev.roman@gmail.com>
2017-05-17 23:18:14 +0300

    fixed test initialization

--- app/src/test/java/com/romazzz/gmclient/ui/login/LoginPresenterTest.java
+++ app/src/test/java/com/romazzz/gmclient/ui/login/LoginPresenterTest.java
@@ -2,28 +2,31 @@ package com.romazzz.gmclient.ui.login;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.verify;
 /**
  * Created by z01tan on 17/05/2017.
  */
 
+@RunWith(MockitoJUnitRunner.class)
 public class LoginPresenterTest {
     @Mock
     LoginView loginView;
     LoginPresenter loginPresenter;
 
     @Before
-    void init(){
+    public void init(){
         MockitoAnnotations.initMocks(this);
         loginPresenter = new LoginPresenter();
         loginPresenter.onAttach(loginView);
     }
 
     @Test
-    void presenterTest() {
+    public void presenterTest() {
         loginPresenter.tryToLogin();
         verify(loginView).showProgress();
     }
