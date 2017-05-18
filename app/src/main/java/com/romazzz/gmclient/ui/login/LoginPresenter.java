6aad7d2be0eef62128db7c7427580c8c24e60232
romaz63n700 <zaytcev.roman@gmail.com>
2017-05-18 21:46:03 +0300

    basic interactions between login presenter and view

--- app/src/main/java/com/romazzz/gmclient/ui/login/LoginPresenter.java
+++ app/src/main/java/com/romazzz/gmclient/ui/login/LoginPresenter.java
@@ -5,28 +5,32 @@ package com.romazzz.gmclient.ui.login;
  */
 
 public class LoginPresenter implements ILoginPresenter {
+    ILoginView mView;
+
     @Override
     public void onAttach(ILoginView view) {
-
+        mView = view;
     }
 
     @Override
     public void onDetach() {
-
+        mView = null;
     }
 
     @Override
     public void tryToLogin() {
-
+        mView.showProgress();
     }
 
     @Override
     public void onLoginSuccess() {
-
+        mView.onLoginSuccess();
+        mView.hideProgress();
     }
 
     @Override
     public void onLoginError() {
-
+        mView.hideProgress();
+        mView.showLoginError();
     }
 }
