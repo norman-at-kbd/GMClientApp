1fa1640d92b1aa628e04d09a7dc17116b87bb92c
romaz63n700 <zaytcev.roman@gmail.com>
2017-05-18 22:38:24 +0300

    added mocked presenter  and test for login view

--- app/src/test/java/com/romazzz/gmclient/ui/login/LoginPresenterTest.java
+++ app/src/test/java/com/romazzz/gmclient/ui/login/LoginPresenterTest.java
@@ -1,5 +1,9 @@
 package com.romazzz.gmclient.ui.login;
 
+import com.romazzz.gmclient.di.component.DaggerViewComponent;
+import com.romazzz.gmclient.di.component.ViewComponent;
+import com.romazzz.gmclient.di.module.ViewModule;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,34 +19,56 @@ import static org.mockito.Mockito.verify;
 @RunWith(MockitoJUnitRunner.class)
 public class LoginPresenterTest {
     @Mock
+    LoginView mockLoginView;
+    @Mock
+    LoginPresenter mockLoginPresenter;
     LoginView loginView;
-
     LoginPresenter loginPresenter;
 
     @Before
     public void init(){
         MockitoAnnotations.initMocks(this);
         loginPresenter = new LoginPresenter();
-        loginPresenter.onAttach(loginView);
+        loginPresenter.onAttach(mockLoginView);
+
+        ViewComponent viewComponent = DaggerViewComponent.builder().viewModule(new ViewModule() {
+            @Override
+            protected ILoginPresenter getLoginPresenter(){
+                return mockLoginPresenter;
+            }
+        }).build();
+
+        loginView = new LoginView() {
+            @Override
+            protected ViewComponent getViewComponent() {
+                return viewComponent;
+            }
+        };
     }
 
     @Test
     public void presenterStartLoginTest() {
         loginPresenter.tryToLogin();
-        verify(loginView).showProgress();
+        verify(mockLoginView).showProgress();
     }
 
     @Test
     public void  presenterLoginSuccesTest() {
         loginPresenter.onLoginError();
-        verify(loginView).hideProgress();
-        verify(loginView).showLoginError();
+        verify(mockLoginView).hideProgress();
+        verify(mockLoginView).showLoginError();
     }
 
     @Test
     public void presenterLoginErrorTest() {
         loginPresenter.onLoginSuccess();
-        verify(loginView).hideProgress();
-        verify(loginView).onLoginSuccess();
+        verify(mockLoginView).hideProgress();
+        verify(mockLoginView).onLoginSuccess();
+    }
+
+    @Test
+    public void viewLoginClickTest() {
+        loginView.loginPressed();
+        verify(mockLoginPresenter).tryToLogin();
     }
 }
