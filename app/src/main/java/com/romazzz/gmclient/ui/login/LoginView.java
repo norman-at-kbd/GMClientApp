d34722cee920f92d874fe2822367287637effcee
romaz63n700 <zaytcev.roman@gmail.com>
2017-05-19 21:14:17 +0300

    login view fixed, now it does inject in onCreate

--- app/src/main/java/com/romazzz/gmclient/ui/login/LoginView.java
+++ app/src/main/java/com/romazzz/gmclient/ui/login/LoginView.java
@@ -1,5 +1,7 @@
 package com.romazzz.gmclient.ui.login;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.romazzz.gmclient.ui.BaseActivity;
@@ -16,6 +18,11 @@ public class LoginView extends BaseActivity implements ILoginView {
 
     public LoginView() {
         super();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         mViewComponent.inject(this);
     }
 
