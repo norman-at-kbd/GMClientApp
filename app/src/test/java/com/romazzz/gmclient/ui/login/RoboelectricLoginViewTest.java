79f35709abf6a1ba1a252f5f53d348bbbe69b225
romazzz <zaytcev.roman@gmail.com>
2017-05-21 20:10:39 +0300

    added class for testing login view with roboelectric

---
+++ app/src/test/java/com/romazzz/gmclient/ui/login/RoboelectricLoginViewTest.java
@@ -0,0 +1,12 @@
+package com.romazzz.gmclient.ui.login;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by z01tan on 21/05/2017.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class RoboelectricLoginViewTest {
+    
+}
