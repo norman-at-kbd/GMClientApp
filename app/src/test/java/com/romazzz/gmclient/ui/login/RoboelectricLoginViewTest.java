7cccb10142f417c1e41743c781f980aa4eeb8f0d
romazzz <zaytcev.roman@gmail.com>
2017-05-21 20:38:19 +0300

    example roboelectric test implementation but it has error: android.content.res.Resources: Unable to find resource ID #0x7f020052 in packages [android, org.robolectric.default]

--- app/src/test/java/com/romazzz/gmclient/ui/login/RoboelectricLoginViewTest.java
+++ app/src/test/java/com/romazzz/gmclient/ui/login/RoboelectricLoginViewTest.java
@@ -1,12 +1,36 @@
 package com.romazzz.gmclient.ui.login;
 
+import android.widget.Button;
+
+import com.romazzz.gmclient.R;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
+import static org.junit.Assert.*;
 /**
  * Created by z01tan on 21/05/2017.
  */
 @RunWith(RobolectricTestRunner.class)
 public class RoboelectricLoginViewTest {
-    
+    private LoginView loginView;
+
+    @Before
+    public void setUp() throws Exception {
+        loginView = Robolectric.buildActivity(LoginView.class).
+                create().
+                resume().
+                get();
+    }
+
+    @Test
+    public void exampleTest() throws Exception{
+        Button button = (Button) loginView.findViewById(R.id.login_btn);
+        String buttonText = button.getText().toString();
+        assertEquals(buttonText, "LOGIN");
+    }
 }
