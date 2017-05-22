09c178a794d2f545e6d2c3533ea8d5d1cc89bd7c
romaz63n700 <zaytcev.roman@gmail.com>
2017-05-22 20:25:17 +0300

    added config annotation to robolectric test class, but looks like actial solution of  [unable to find resource ID] was addition of working directory MODULE_DIR(look into robolectric official doc)

--- app/src/test/java/com/romazzz/gmclient/ui/login/RoboelectricLoginViewTest.java
+++ app/src/test/java/com/romazzz/gmclient/ui/login/RoboelectricLoginViewTest.java
@@ -2,6 +2,7 @@ package com.romazzz.gmclient.ui.login;
 
 import android.widget.Button;
 
+import com.romazzz.gmclient.BuildConfig;
 import com.romazzz.gmclient.R;
 
 import org.junit.Before;
@@ -10,12 +11,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 /**
  * Created by z01tan on 21/05/2017.
  */
 @RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class RoboelectricLoginViewTest {
     private LoginView loginView;
 
