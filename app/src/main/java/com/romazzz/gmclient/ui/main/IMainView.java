9ca89b4b0c336f3cce3222226069393d3c218d8e
romaz63n700 <zaytcev.roman@gmail.com>
2017-05-25 21:31:16 +0300

    main view interface changed

--- app/src/main/java/com/romazzz/gmclient/ui/main/IMainView.java
+++ app/src/main/java/com/romazzz/gmclient/ui/main/IMainView.java
@@ -1,6 +1,9 @@
 package com.romazzz.gmclient.ui.main;
 
+import com.romazzz.gmclient.mailclient.IMessage;
 import com.romazzz.gmclient.ui.IMVPView;
+
+import java.util.Collection;
 
 /**
  * Created by z01tan on 5/15/17.
@@ -8,6 +11,7 @@ import com.romazzz.gmclient.ui.IMVPView;
 
 public interface IMainView extends IMVPView {
     void loginPressed();
-    void showLoginError();
+    void showError(Throwable error);
     void onLoginSuccess();
+    void showMessages(Collection<IMessage> messages);
 }
