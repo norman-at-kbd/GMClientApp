a729c5dd33ea7373b5e4cdf9a2b610a75f047fd3
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-02 20:29:41 +0300

    message builder for test purposes

---
+++ app/src/test/java/com/romazzz/gmclient/mailclient/TestMessageBuilder.java
@@ -0,0 +1,14 @@
+package com.romazzz.gmclient.mailclient;
+
+import java.util.Collection;
+
+/**
+ * Created by z01tan on 6/2/17.
+ */
+
+public class TestMessageBuilder {
+
+    Collection<IMessage> getMessageList() {
+
+    }
+}
