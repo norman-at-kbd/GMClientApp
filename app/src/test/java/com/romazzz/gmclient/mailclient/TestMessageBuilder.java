2b69671f8644252fb858e82629f5b9785bf8aad9
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-02 21:05:55 +0300

    implemented test message builder

--- app/src/test/java/com/romazzz/gmclient/mailclient/TestMessageBuilder.java
+++ app/src/test/java/com/romazzz/gmclient/mailclient/TestMessageBuilder.java
@@ -1,5 +1,6 @@
 package com.romazzz.gmclient.mailclient;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -8,7 +9,97 @@ import java.util.Collection;
 
 public class TestMessageBuilder {
 
-    Collection<IMessage> getMessageList() {
+    static class MessageBuilder {
+        static private int COUNT;
+        boolean isSent = false;
+        boolean isUnread = false;
+        String id = "defaultId";
+        String from = "defaultFrom";
+        String to = "defaultTo";
+        String subject = "defaultSubject";
+        String text = "defaultText";
 
+        public MessageBuilder() {
+            COUNT = 0;
+        }
+        public void isSent(boolean issent) {
+            this.isSent = issent;
+        }
+
+        public void isUnread(boolean isunread) {
+            this.isUnread = isunread;
+        }
+
+        public void id(String id) {
+            this.id = id;
+        }
+
+        public void from(String from) {
+            this.from = from;
+        }
+
+        public void to(String to) {
+            this.to = to;
+        }
+
+        public void subject(String subject) {
+            this.subject = subject;
+        }
+
+        public void text(String text) {
+            this.text = text;
+        }
+
+        public IMessage build() {
+            COUNT++;
+            return new IMessage() {
+                @Override
+                public boolean isSent() {
+                    return isSent;
+                }
+
+                @Override
+                public boolean isUnread() {
+                    return isUnread;
+                }
+
+                @Override
+                public String getID() {
+                    return id + "_" + COUNT;
+                }
+
+                @Override
+                public String getFrom() {
+                    return from;
+                }
+
+                @Override
+                public String getTo() {
+                    return to;
+                }
+
+                @Override
+                public String getSubject() {
+                    return subject;
+                }
+
+                @Override
+                public String getText() {
+                    return text;
+                }
+            };
+        }
+    }
+
+    public IMessage getMessage() {
+        return new MessageBuilder().build();
+    }
+
+    public Collection<IMessage> getMessageList(int size) {
+        MessageBuilder builder = new MessageBuilder();
+        ArrayList<IMessage> messages = new ArrayList<>();
+        for(int i=0; i<size; i++)
+            messages.add(builder.build());
+        return messages;
     }
 }
