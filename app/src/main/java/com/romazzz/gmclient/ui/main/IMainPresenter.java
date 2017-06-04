d4f915592930931cd2e57a965d43649f21a0f009
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-04 20:06:01 +0300

    new method to receive onActivityResult forwarded from MainView

--- app/src/main/java/com/romazzz/gmclient/ui/main/IMainPresenter.java
+++ app/src/main/java/com/romazzz/gmclient/ui/main/IMainPresenter.java
@@ -1,5 +1,7 @@
 package com.romazzz.gmclient.ui.main;
 
+import android.content.Intent;
+
 import com.romazzz.gmclient.mailclient.IMessage;
 import com.romazzz.gmclient.ui.IMVPPresenter;
 
@@ -13,4 +15,5 @@ public interface IMainPresenter extends IMVPPresenter <IMainView> {
     void requestMessages();
     void onRequestMessagesError(Throwable throwable);
     void onRequestMessagesSuccess(Collection<IMessage> messages);
+    void onActivityResult(int requestCode, int resultCode, Intent data);
 }
