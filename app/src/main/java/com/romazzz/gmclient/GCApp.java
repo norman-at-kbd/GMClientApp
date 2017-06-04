5be2d97684eb4c08be282a90d604fe8b250d7366
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-04 19:45:16 +0300

    google play services consts now lay in GCApp

--- app/src/main/java/com/romazzz/gmclient/GCApp.java
+++ app/src/main/java/com/romazzz/gmclient/GCApp.java
@@ -8,6 +8,11 @@ import android.content.Context;
  */
 
 public class GCApp extends Application {
+    public static final int REQUEST_ACCOUNT_PICKER = 1000;
+    public static final int REQUEST_AUTHORIZATION = 1001;
+    public static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    public static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+
     private static GCApp APP;
     private GCApp() {}
 
