3cdbb18ffbc41d5b6851443a7be986aa197d81e0
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-04 21:00:18 +0300

    onRequestPermissionsResult override sets presenter as handler

--- app/src/main/java/com/romazzz/gmclient/ui/main/MainView.java
+++ app/src/main/java/com/romazzz/gmclient/ui/main/MainView.java
@@ -3,6 +3,7 @@ package com.romazzz.gmclient.ui.main;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.widget.Button;
 
@@ -19,6 +20,7 @@ import javax.inject.Inject;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import pub.devrel.easypermissions.EasyPermissions;
 
 /**
  * Created by z01tan on 5/16/17.
@@ -104,5 +106,12 @@ public class MainView extends BaseActivity implements IMainView {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         mPresenter.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(
+                requestCode, permissions, grantResults, mPresenter);
     }
 }
