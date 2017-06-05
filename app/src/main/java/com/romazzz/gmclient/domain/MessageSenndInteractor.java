01c4da013f3696fa18052e68ef0e9922391ef625
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-05 20:57:48 +0300

    Message send interactor implementation

---
+++ app/src/main/java/com/romazzz/gmclient/domain/MessageSenndInteractor.java
@@ -0,0 +1,22 @@
+package com.romazzz.gmclient.domain;
+
+import android.util.Log;
+
+import com.google.api.services.gmail.model.Message;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import rx.Completable;
+
+/**
+ * Created by z01tan on 6/5/17.
+ */
+
+public class MessageSenndInteractor implements IMessageSendInteractor {
+    private static final String TAG = GetMessageListInteractor.class.getSimpleName();
+
+
+}
