f332f460527f04184c6652824503c67bd576c440
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-05 21:10:33 +0300

    getSenderCompletable method added to IMessageSendInetactor interface

--- app/src/main/java/com/romazzz/gmclient/domain/IMessageSendInteractor.java
+++ app/src/main/java/com/romazzz/gmclient/domain/IMessageSendInteractor.java
@@ -1,8 +1,11 @@
 package com.romazzz.gmclient.domain;
 
+import rx.Completable;
+
 /**
  * Created by z01tan on 5/3/17.
  */
 
 public interface IMessageSendInteractor {
+    Completable getSenderCompletable();
 }
