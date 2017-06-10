27eef687481768b464015e6fbcb01d5cf4e4d661
romazzz <zaytcev.roman@gmail.com>
2017-06-10 22:43:52 +0300

    BaseActivity now mixes in AppComponent into ViewComponent

--- app/src/main/java/com/romazzz/gmclient/ui/BaseActivity.java
+++ app/src/main/java/com/romazzz/gmclient/ui/BaseActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.romazzz.gmclient.GCApp;
+import com.romazzz.gmclient.di.component.AppComponent;
 import com.romazzz.gmclient.di.component.DaggerViewComponent;
 import com.romazzz.gmclient.di.component.ViewComponent;
 import com.romazzz.gmclient.di.module.ViewModule;
+import com.romazzz.gmclient.ui.main.MainPresenter;
 
 /**
  * Created by z01tan on 5/16/17.
@@ -15,6 +18,8 @@ import com.romazzz.gmclient.di.module.ViewModule;
 public class BaseActivity extends AppCompatActivity {
 
     protected ViewComponent getViewComponent() {
-        return DaggerViewComponent.builder().viewModule(new ViewModule()).build();
+        return DaggerViewComponent.builder()
+                .viewModule(new ViewModule()).
+                        appComponent(GCApp.getInstance().getAppComponent()).build();
     }
 }
