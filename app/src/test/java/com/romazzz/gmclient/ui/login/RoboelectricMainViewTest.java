0146a59ce7ff658f786b4628ef30e19a3b003496
romazzz <zaytcev.roman@gmail.com>
2017-06-13 21:48:05 +0300

    experimenting with robolectric and mockito combination

--- app/src/test/java/com/romazzz/gmclient/ui/login/RoboelectricMainViewTest.java
+++ app/src/test/java/com/romazzz/gmclient/ui/login/RoboelectricMainViewTest.java
@@ -3,13 +3,20 @@ package com.romazzz.gmclient.ui.login;
 import android.widget.Button;
 
 import com.romazzz.gmclient.BuildConfig;
+import com.romazzz.gmclient.GCApp;
 import com.romazzz.gmclient.R;
+import com.romazzz.gmclient.di.component.DaggerViewComponent;
+import com.romazzz.gmclient.di.component.ViewComponent;
+import com.romazzz.gmclient.di.module.ViewModule;
+import com.romazzz.gmclient.ui.main.IMainPresenter;
+import com.romazzz.gmclient.ui.main.MainPresenter;
 import com.romazzz.gmclient.ui.main.MainView;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
@@ -22,13 +29,25 @@ import static org.junit.Assert.*;
 @Config(constants = BuildConfig.class)
 public class RoboelectricMainViewTest {
     private MainView MainView;
-
+    @Mock
+    private MainPresenter mockedMainPresenter;
     @Before
     public void setUp() throws Exception {
         MainView = Robolectric.buildActivity(com.romazzz.gmclient.ui.main.MainView.class).
                 create().
                 resume().
                 get();
+
+        ViewComponent viewComponent = DaggerViewComponent.builder()
+                .viewModule(new ViewModule() {
+                    @Override
+                    protected IMainPresenter getMainPresenter () {
+                        return mockedMainPresenter;
+                    }
+                })
+                .appComponent(GCApp.getInstance().getAppComponent())
+                .build();
+        viewComponent.inject(MainView);
     }
 
     @Test
