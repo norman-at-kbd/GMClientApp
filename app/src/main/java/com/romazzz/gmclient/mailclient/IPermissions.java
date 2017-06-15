14cceed10193726ae9b953d9c7bd5ef07c419df6
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-15 23:22:16 +0300

    intnerface for easypermissions interaction

---
+++ app/src/main/java/com/romazzz/gmclient/mailclient/IPermissions.java
@@ -0,0 +1,10 @@
+package com.romazzz.gmclient.mailclient;
+
+/**
+ * Created by z01tan on 6/15/17.
+ */
+
+public interface IPermissions {
+    void chooseAccount();
+    void requestPermissions();
+}
