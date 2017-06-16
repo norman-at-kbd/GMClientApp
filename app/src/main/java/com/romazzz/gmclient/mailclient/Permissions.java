080b9f3c09d5a402793e8e655923dffe488afad8
romazzz <zaytcev.roman@gmail.com>
2017-06-16 16:02:43 +0300

    permissions basic implementation

---
+++ app/src/main/java/com/romazzz/gmclient/mailclient/Permissions.java
@@ -0,0 +1,17 @@
+package com.romazzz.gmclient.mailclient;
+
+/**
+ * Created by z01tan on 16/06/2017.
+ */
+
+public class Permissions implements IPermissions {
+    @Override
+    public void chooseAccount() {
+
+    }
+
+    @Override
+    public void requestPermissions() {
+
+    }
+}
