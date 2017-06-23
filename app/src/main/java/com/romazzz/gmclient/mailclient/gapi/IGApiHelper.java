5e43667f28cf66b7be95355a3e9554e03e05b322
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-23 12:08:45 +0300

    facade interface for google api login interaction

---
+++ app/src/main/java/com/romazzz/gmclient/mailclient/gapi/IGApiHelper.java
@@ -0,0 +1,8 @@
+package com.romazzz.gmclient.mailclient.gapi;
+
+/**
+ * Created by z01tan on 6/23/17.
+ */
+
+public interface IGApiHelper extends IGApiAvalability, IPermissions, ICredentialsProvider {
+}
