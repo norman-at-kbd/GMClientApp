d2a61ab91991d25ba1e78544c7fea2077638aabb
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-26 22:05:03 +0300

    common interface for mail client is changed

--- app/src/main/java/com/romazzz/gmclient/mailclient/ICommonMailClient.java
+++ app/src/main/java/com/romazzz/gmclient/mailclient/ICommonMailClient.java
@@ -1,5 +1,6 @@
 package com.romazzz.gmclient.mailclient;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -8,6 +9,6 @@ import java.util.List;
 
 public interface ICommonMailClient {
     void login();
-    void send(IMessage message);
+    void send(IMessage message) throws IOException;
     List<IMessage> getList();
 }
