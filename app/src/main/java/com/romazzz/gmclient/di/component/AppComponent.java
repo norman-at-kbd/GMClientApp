a2a85f00c148b947d0e889e143d4a597da45a161
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-27 21:59:45 +0300

    app component provides every entity which has to exist appwide

--- app/src/main/java/com/romazzz/gmclient/di/component/AppComponent.java
+++ app/src/main/java/com/romazzz/gmclient/di/component/AppComponent.java
@@ -2,10 +2,15 @@ package com.romazzz.gmclient.di.component;
 
 import com.romazzz.gmclient.di.PerAppScope;
 import com.romazzz.gmclient.di.module.AppModule;
+import com.romazzz.gmclient.di.module.MailClientModule;
+import com.romazzz.gmclient.di.module.MessageStorageModule;
 import com.romazzz.gmclient.di.module.NetworkMailClientModule;
 import com.romazzz.gmclient.domain.IGetMessageListInteractor;
+import com.romazzz.gmclient.domain.IMessageSendInteractor;
+import com.romazzz.gmclient.mailclient.IMailClient;
 import com.romazzz.gmclient.mailclient.gapi.ICredentialsProvider;
 import com.romazzz.gmclient.mailclient.gapi.IGApiHelper;
+import com.romazzz.gmclient.mailclient.localclient.IMessageStorage;
 import com.romazzz.gmclient.mailclient.network.INetworkMailClient;
 import com.romazzz.gmclient.ui.main.MainPresenter;
 
@@ -15,12 +20,17 @@ import dagger.Component;
  * Created by z01tan on 5/29/17.
  */
 
-@Component(modules = {AppModule.class, NetworkMailClientModule.class})
+@Component(modules = {AppModule.class,
+        MailClientModule.class,
+        NetworkMailClientModule.class,
+        MessageStorageModule.class})
 @PerAppScope
 public interface AppComponent {
     void inject(MainPresenter mainPresenter);
     ICredentialsProvider getCredentialsProvider();
     IGetMessageListInteractor getMessageListInteractor();
     IGApiHelper getGApiHelper();
-    INetworkMailClient getNetworkMailClient();
+    IMailClient getMailClient();
+    IMessageSendInteractor getMessageSendInteractor();
+    IMessageStorage getMessageStorage();
 }
