b66a48f8be609d4edcca873156a74cc9632e947d
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-27 21:12:39 +0300

    AppComponent now provides network mail client

--- app/src/main/java/com/romazzz/gmclient/di/component/AppComponent.java
+++ app/src/main/java/com/romazzz/gmclient/di/component/AppComponent.java
@@ -2,9 +2,11 @@ package com.romazzz.gmclient.di.component;
 
 import com.romazzz.gmclient.di.PerAppScope;
 import com.romazzz.gmclient.di.module.AppModule;
+import com.romazzz.gmclient.di.module.NetworkMailClientModule;
 import com.romazzz.gmclient.domain.IGetMessageListInteractor;
 import com.romazzz.gmclient.mailclient.gapi.ICredentialsProvider;
 import com.romazzz.gmclient.mailclient.gapi.IGApiHelper;
+import com.romazzz.gmclient.mailclient.network.INetworkMailClient;
 import com.romazzz.gmclient.ui.main.MainPresenter;
 
 import dagger.Component;
@@ -13,11 +15,12 @@ import dagger.Component;
  * Created by z01tan on 5/29/17.
  */
 
-@Component(modules = {AppModule.class})
+@Component(modules = {AppModule.class, NetworkMailClientModule.class})
 @PerAppScope
 public interface AppComponent {
     void inject(MainPresenter mainPresenter);
     ICredentialsProvider getCredentialsProvider();
     IGetMessageListInteractor getMessageListInteractor();
     IGApiHelper getGApiHelper();
+    INetworkMailClient getNetworkMailClient();
 }
