445611972d38e2b7bfba08c078667243d4eaaf86
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-27 22:27:12 +0300

    log instead of request

--- app/src/main/java/com/romazzz/gmclient/ui/main/MainPresenter.java
+++ app/src/main/java/com/romazzz/gmclient/ui/main/MainPresenter.java
@@ -103,7 +103,8 @@ public class MainPresenter implements IMainPresenter {
 //                    subscribeOn(Schedulers.io()).
 //                    observeOn(AndroidSchedulers.mainThread()).
 //                    subscribe(new GetMessageObserver());
-            sendMessageTestMethod();
+//            sendMessageTestMethod();
+            Log.d(TAG, "SUCCESS");
         }
     }
 
