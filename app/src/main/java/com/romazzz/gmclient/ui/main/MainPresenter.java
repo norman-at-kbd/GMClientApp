093f0b486b3c37b2c8224fff55f32dd738de524e
romaz63n700 <zaytcev.roman@gmail.com>
2017-06-29 22:03:31 +0300

    fixed work with credentials

--- app/src/main/java/com/romazzz/gmclient/ui/main/MainPresenter.java
+++ app/src/main/java/com/romazzz/gmclient/ui/main/MainPresenter.java
@@ -93,7 +93,7 @@ public class MainPresenter implements IMainPresenter {
     public void requestMessages() {
         if (!mGApiHelper.isGooglePlayServicesAvailable()) {
             mGApiHelper.acquireGooglePlayServices();
-        } else if (mGApiHelper.getAccountName() == null) {
+        } else if (mGApiHelper.getCredentials().getSelectedAccountName() == null) {
             chooseAccount();
         } else {
             getMessageListInteractor.getMessagesList().
