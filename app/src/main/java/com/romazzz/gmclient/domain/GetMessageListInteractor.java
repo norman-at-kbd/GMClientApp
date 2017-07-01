a2b08f0f91481d7396bc9e3038355d37b51ebdb7
romaz63n700 <zaytcev.roman@gmail.com>
2017-07-01 23:11:55 +0300

    working on getmessagesinteractor

--- app/src/main/java/com/romazzz/gmclient/domain/GetMessageListInteractor.java
+++ app/src/main/java/com/romazzz/gmclient/domain/GetMessageListInteractor.java
@@ -2,22 +2,36 @@ package com.romazzz.gmclient.domain;
 
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.romazzz.gmclient.mailclient.IMessage;
+import com.romazzz.gmclient.mailclient.network.INetworkMailClient;
 
+import java.io.IOException;
 import java.util.Collection;
 
+import javax.inject.Inject;
+
 import rx.Observable;
+import rx.Subscriber;
 
 /**
  * Created by z01tan on 6/4/17.
  */
 
 public class GetMessageListInteractor implements IGetMessageListInteractor {
-    public GetMessageListInteractor() {
-
+    INetworkMailClient mNMailClient;
+    @Inject
+    public GetMessageListInteractor(INetworkMailClient client) {
+        mNMailClient = client;
     }
 
     @Override
     public Observable<Collection<IMessage>> getMessagesList() {
-        return null;
+        return Observable.create(subscriber ->  {
+                try {
+                    subscriber.onNext(mNMailClient.getList());
+                } catch (IOException e) {
+                    subscriber.onError(e);
+                }
+                subscriber.onCompleted();
+        });
     }
 }
