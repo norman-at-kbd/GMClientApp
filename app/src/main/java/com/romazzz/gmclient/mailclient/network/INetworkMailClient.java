f252308c11c70c6220ed8a1c4dc2375946248702
romaz63n700 <zaytcev.roman@gmail.com>
2017-07-04 21:00:42 +0300

    network mail client interface now have googleapi specific methods

--- app/src/main/java/com/romazzz/gmclient/mailclient/network/INetworkMailClient.java
+++ app/src/main/java/com/romazzz/gmclient/mailclient/network/INetworkMailClient.java
@@ -1,7 +1,11 @@
 package com.romazzz.gmclient.mailclient.network;
 
+import com.google.api.services.gmail.model.Message;
 import com.romazzz.gmclient.mailclient.ICommonMailClient;
 import com.romazzz.gmclient.mailclient.IMessage;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by z01tan on 4/6/17.
@@ -9,4 +13,6 @@ import com.romazzz.gmclient.mailclient.IMessage;
 
 public interface INetworkMailClient extends ICommonMailClient{
     void delete(String messageID);
+    List<Message> getGoogleMessageList() throws IOException;
+    Message getMessageById(String messageId) throws IOException;
 }
