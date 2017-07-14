e5955ddf278ea7df3dc46770b95b4e0e74c250a2
romaz63n700 <zaytcev.roman@gmail.com>
2017-07-14 22:21:53 +0300

    working on message converter

--- app/src/main/java/com/romazzz/gmclient/domain/GoogleMessageToIMessageConverter.java
+++ app/src/main/java/com/romazzz/gmclient/domain/GoogleMessageToIMessageConverter.java
@@ -20,26 +20,26 @@ public class GoogleMessageToIMessageConverter {
         String to = "";
         String subject = "";
         String text = "";
-        String snippet = "";
+        String snippet = message.getSnippet();
         try {
             Log.d("MESSAGES", "msg: " + message.toPrettyString());
             for (MessagePartHeader header : message.getPayload().getHeaders()) {
                 if (header.getName().equals("From"))
-                    Log.d("MESSAGES", "From : " + header.getValue());
+                    from = header.getValue();
                 if (header.getName().equals("To"))
-                    Log.d("MESSAGES", "To : " + header.getValue());
+                    to = header.getValue();
                 if (header.getName().equals("Subject"))
-                    Log.d("MESSAGES", "Subject : " + header.getValue());
-//                            Log.d("MESSAGES", "header : " + header.getName() + " value: " + header.getValue());
+                    subject = header.getValue();
             }
-            if (message.getPayload().getBody().getSize() != 0)
-                Log.d("MESSAGES", "body " + new String(Base64.decode(message.getPayload().getBody().getData(), Base64.DEFAULT), Charset.forName("UTF-8")));
-            Log.d("MESSAGES", "payload headers: " + message.getPayload().getHeaders().toString());
-            Log.d("MESSAGE", "message " + message.getSnippet());
+//            if (message.getPayload().getBody().getSize() != 0)
+//                Log.d("MESSAGES", "body " + new String(Base64.decode(message.getPayload().getBody().getData(), Base64.DEFAULT), Charset.forName("UTF-8")));
+//            Log.d("MESSAGES", "payload headers: " + message.getPayload().getHeaders().toString());
+//            Log.d("MESSAGE", "message " + message.getSnippet());
             for (MessagePart part : message.getPayload().getParts()) {
                 if (part.getMimeType().equals("text/plain"))
-                    Log.d("PART_HEADERS", "" + part.getHeaders());
-                Log.d("PART", "part: " + new String(part.getBody().decodeData()));
+                    text += new String(part.getBody().decodeData());
+//                    Log.d("PART_HEADERS", "" + part.getHeaders());
+//                Log.d("PART", "part: " + new String(part.getBody().decodeData()));
             }
         } catch (Exception e) {
             Log.d("message: ", "Exception " + e.toString());
